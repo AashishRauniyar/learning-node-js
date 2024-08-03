ace274e7c41633ce948f1ceeca7601760e429f16
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-08-04 00:27:55 +0545

    learning-node-js-day1

---
+++ index.js
@@ -0,0 +1,11 @@
+import  getPosts, {getPostLength} from "./postController.js";
+
+console.log(getPosts());
+console.log(getPostLength());
+
+
+// const {generateRandomNumber, celciusToFahrenheit} = require('./utils');
+
+// console.log(generateRandomNumber()); // 42
+
+// console.log(celciusToFahrenheit(100)); // 212
---
+++ postController.js
@@ -0,0 +1,11 @@
+const posts = [
+    {id:1, title: 'Post 1', content: 'This is post 1'},
+    {id:2, title: 'Post 2', content: 'This is post 2'},
+]
+
+const getPosts = () => posts;
+
+export const getPostLength = () => posts.length;
+
+
+export default getPosts
