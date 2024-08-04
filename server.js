acff72f8f633f2eec54f11d40ac816c423611adf
AashishRauniyar <rauniyaaraashish@gmail.com>
2024-08-04 13:45:41 +0545

    routing-get-post

--- server.js
+++ server.js
@@ -1,15 +1,51 @@
 import http from 'http';
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path';
 const PORT = process.env.PORT;
 
-const server = http.createServer((req, res) => {
+//Get current path
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-    // res.setHeader('Content-Type', 'text/plain');
-    // res.statusCode = 404;
+console.log(__filename);
+console.log(__dirname);
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
 
-    res.end(JSON.stringify({message: 'Server error'}));
-    //res.end('<h1>hello world</h1>');
+
+const server = http.createServer(async (req, res) => {
+
+    try {
+        // check if GET request
+
+        if(req.method === 'GET'){
+
+            let filePath;
+
+            if(req.url === '/'){
+                filePath = path.join(__dirname, 'public', 'index.html');
+            } else if(req.url === '/about'){
+                filePath = path.join(__dirname, 'public', 'about.html');
+            } else{
+                filePath = path.join(__dirname, 'public', '404.html');
+            }
+
+            const data = await fs.readFile(filePath);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        }else{
+            throw new Error('Method not allowed');
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/html'});
+                res.end('<h1>Server Error</h1>');
+    }
+
+    
+    
 });
+
+
 
 server.listen(PORT, ()=>{console.log(`server is running on port ${PORT}`)});
